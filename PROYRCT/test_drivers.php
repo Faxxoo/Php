b0b9134ab2c72bbaba6a9bd1fcd71b536741b391
Faxxoo <fachx32@outlook.com>
2026-06-01 18:49:50 -0400

    Delete PROYRCT directory

--- PROYRCT/test_drivers.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "PHP Version: " . phpversion() . "\n";
-echo "Loaded php.ini: " . php_ini_loaded_file() . "\n";
-echo "Available Drivers: " . implode(', ', PDO::getAvailableDrivers()) . "\n";
-?>
